5459b5eb2ac8f9e76a85236d36a316f4c2e2f33c
Mike Roberts <mike.roberts.2k10@googlemail.com>
2022-12-28 20:12:49 +0000

    Initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# breakpoint-highlighter README
---
+++ src/extension.ts
@@ -0,0 +1,49 @@
+import * as vscode from 'vscode';
+
+const breakpointDecorationType = vscode.window.createTextEditorDecorationType({
+	isWholeLine: true,
+    backgroundColor: 'rgba(255, 0, 0, 0.2)',
+    after: {
+        color: "#FF0000",
+    }
+});
+
+function updateDecorations() {
+    const editor = vscode.window.activeTextEditor;
+    if (!editor) {
+        return;
+    }
+
+    const breakpoints = vscode.debug.breakpoints;
+	const docPath = editor.document.uri.toString();
+
+    const decorations = [];
+    for (const b of breakpoints as vscode.SourceBreakpoint[]) {
+        if (b.location === undefined || b.location.uri.toString() !== docPath) {
+            continue;
+        }
+
+        const line = b.location.range.start.line;
+        const range = new vscode.Range(line, 0, line, 0);
+        var decoration = {range: range} as vscode.DecorationOptions;
+
+        if (b.condition) {
+            decoration.renderOptions = {
+                after: {
+                    contentText: " " + b.condition,
+                },
+            };
+        }
+        decorations.push(decoration);
+    }
+
+    editor.setDecorations(breakpointDecorationType, decorations);
+}
+
+export function activate(context: vscode.ExtensionContext) {
+	vscode.window.onDidChangeActiveTextEditor(updateDecorations);
+	vscode.debug.onDidChangeBreakpoints(updateDecorations);
+	updateDecorations();
+}
+
+export function deactivate() {}
